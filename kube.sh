824902d50c391aeffbfa0eed9bad84ba37b81166
Anggara Pratama <138255761+apgaara@users.noreply.github.com>
2025-03-07 08:54:27 +0700

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -1,2 +1,6 @@
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.26/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+kubeadm join 10.10.101.151:6443 --token 49xn3h.k79axgfkw9og93jr \
+    --discovery-token-ca-cert-hash sha256:df33f07aa292c7284c6e7d2453f1f22662798bdf5ef00e6cb5db3bb92a3f35a \
+    --control-plane --certificate-key 040d4d492a763e064a3c10084ad26cd46c2ff4e742b23b26162be940c30034d2
+
+kubeadm join 10.10.101.151:6443 --token 49xn3h.k79axgfkw9og93jr \
+    --discovery-token-ca-cert-hash sha256:df33f07aa292c7284c6e7d2453f1f22662798bdf5ef00e6cb5db3bb92a3f35a
